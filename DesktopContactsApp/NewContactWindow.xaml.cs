5476e0a583c96931add252d2c0bbf2cc2363c734
Ty Day <tyrday@gmail.com>
2019-10-20 16:15:38 -0400

    Db Connection works both ways

--- DesktopContactsApp/NewContactWindow.xaml.cs
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -35,11 +35,8 @@ namespace DesktopContactsApp
                 Email = emailTextBox.Text,
                 Phone = phoneTextBox.Text
             };
-            string databaseName = "Contacts.db";
-            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string databasePath = System.IO.Path.Combine(folderPath, databaseName);
 
-            using (SQLiteConnection connection = new SQLiteConnection(databasePath))
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
             {
                 connection.CreateTable<Contact>();
                 connection.Insert(contact);
